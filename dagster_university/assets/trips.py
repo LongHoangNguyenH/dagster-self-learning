fbe9b3bbb81a0fbc7efa2302027daa9934ab1e4a
LongHoangNguyenH <longnh.uit@gmail.com>
2024-08-14 09:47:36 +0700

    create taxi_zone asset and query taxi_trip

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -1,6 +1,8 @@
 import requests
 from . import constants
 from dagster import asset
+import os
+import duckdb
 
 @asset
 def taxi_trips_file() -> None:
@@ -11,3 +13,38 @@ def taxi_trips_file() -> None:
     )
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), 'wb') as output_file:
         output_file.write(raw_trips.content)	
+
+@asset(deps=[taxi_trips_file])
+def taxi_trips()->None:
+    """
+      The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    sql_query = """
+        create or replace table trips as (
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount
+          from 'data/raw/taxi_trips_2023-03.parquet'
+        );
+    """
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    conn.execute(sql_query)
+
+@asset
+def taxi_zones_file() -> None:
+    """
+      The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
+    """
+    raw_zones = requests.get(
+       "https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+    )
+    with open(constants.TAXI_ZONES_FILE_PATH, 'wb') as output_file:
+        output_file.write(raw_zones.content)
